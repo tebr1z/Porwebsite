eceb4f4a84886f9956e1e623b239a1450fd2260e
tebr1z <tabrizyh@code.edu.az>
2024-06-12 13:54:16 +0400

    Feat : Portfolya

---
+++ foodorder/assets/app.js
@@ -0,0 +1,44 @@
+// let array = [1,2,3,4,5,6,22,11,33,44,22];
+
+// let count = 0;
+
+// for (let index = 2; index < array; index++) {
+//    if (n%index==0) {
+//     count++;
+//    }
+//    console.log(count );
+
+// }
+
+// let array = [ 2, 3, 4, 5, 6, 22, 11, 33, 44, 22,];
+
+// let bool = true;
+
+// for (let index = 2; index < array.length; index++) {
+//   if (array[index] % index == 1) {
+//     count++;
+//   }
+// console.log("Sade ədədlərin sayı:", count);
+
+// }
+
+
+// function GetRandom(max){
+//     return Math.floor(Math.random() * max);
+// }
+// const GetRandom = [];
+
+// const count = GetRandom.push(23);
+
+// Math.floor(Math.random() * GetRandom);
+
+// console.log(GetRandom(count));
+const GetRandom = [];
+GetRandom.push(22);
+
+const randomIndex = Math.floor(Math.random() * GetRandom.length);
+const randomElement = GetRandom[randomIndex];
+
+console.log("Təsadüfi element:", randomElement);
+
+getEle
